4c990c7ef33b0d1accb113285dcef09d333cac24
yjnoh1016 <yjnoh1016@naver.com>
2021-01-12 22:33:23 +0900

    210112(2) React JS movie-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 
-class App extends React.Component{
-  state = {
-    isLoding: true,
-    movie: []
-  };
-  componentDidMount(){
-    setTimeout(() => {
-      this.setState({isLoding : false});
-    }, 6000);
-  }
-  render(){
-    const {isLoding} = this.state;
-    return(
-      <div>
-        {isLoding ? "Loading..." : "We are ready"}
-      </div>
-    );
-  }
-
+function App() {
+  return(
+    <HashRouter>
+      <Navigation/>
+      <Route path="/" exact={true} component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/movie/:id" component={Detail}/>
+    </HashRouter>
+  );
 }
 
 export default App;
